c9c592d10abbf0ac2fa4bfd7f97acf69d1ad8fa9
WesleyLiaw <wesleyliaw777@gmail.com>
2023-06-06 14:50:36 -0500

    fixed experience

--- src/config/resume/experienceConfig.js
+++ src/config/resume/experienceConfig.js
@@ -3,9 +3,9 @@ export const experienceConfig = [
     name: "Web Development Agency",
     title: "Junior Frontend Developer",
     description: [
-      "Transformed a completely static HTML, CSS, JavaScript and React single page application into an interactive user interface using animations, transitions and carousels",
-      "Processed API requests with Axios to dynamically represent data from a cloud server and represented it through skeleton loading states, pagination and dynamic routing",
-      "Utilized Git version control and the GitHub interface to work and thrive in a virtual and collaborative team environment",
+      "Built responsive websites for diverse businesses, employing HTML, CSS, JavaScript, and React best practices",
+      "Achieved a significant 15% improvement in webpage performance by implementing React Links, skeleton loading states, and lazy loading techniques",
+      "Consistently delivered high-quality solutions under tight deadlines through effective problem-solving, task prioritization, and close collaboration with team members and stakeholders",
     ],
     date: "May 2022 - Present",
   },
@@ -20,5 +20,4 @@ export const experienceConfig = [
     ],
     date: "Dec 2021 - May 2022",
   },
-  
 ];
